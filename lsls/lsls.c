5f31f1ee29c99a8e53f2f9babd5452dd030f48e3
Marious <mlancelot88@gmail.com>
2019-06-07 11:52:11 -0500

    MVP complete. Succesful compile and print out of `lsls` directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 /**
  * Main
@@ -7,12 +8,39 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  char *dir_name;
+  DIR *od;
+  struct dirent *enter;
+  struct stat fileStat;
+
+  if (argc <= 1) // checks to see if there is no input
+  {
+    dir_name = "."; //prints current directory
+  } else if (argc == 2) { // if the directory in input is (argv[1]), make a reference to it
+    dir_name = argv[1];
+  }
+  else { //catches incorrect directories or errors
+    printf("Cannot open dir '%s'!", argv[1]);
+  }
 
   // Open directory
+  od = opendir(dir_name);
 
-  // Repeatly read and print entries
+  // Repeatedly read and print entries
+  while ((enter = readdir(od)) != NULL)
+  {
+    stat(enter->d_name, &fileStat);
+    if (fileStat.st_mode & __S_IFDIR)
+    {
+      printf("%s\n", enter->d_name);
+    }
+    else {
+      printf("size: %ldkb\t file: %s\n", fileStat.st_size, enter->d_name);
+    }
+  }
 
   // Close directory
+  closedir(od);
 
   return 0;
 }
