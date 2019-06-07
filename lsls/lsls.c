a7186a3df2482f58a053187ca69e67d24928c33f
Marious <mlancelot88@gmail.com>
2019-06-07 18:19:10 -0500

    Changes made to MVP. Terminal now throws back errors for incorrect directories

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <stdlib.h>
 
 /**
  * Main
@@ -9,7 +10,6 @@ int main(int argc, char **argv)
 {
   // Parse command line
   char *dir_name;
-  DIR *od;
   struct dirent *enter;
   struct stat fileStat;
 
@@ -19,12 +19,16 @@ int main(int argc, char **argv)
   } else if (argc == 2) { // if the directory in input is (argv[1]), make a reference to it
     dir_name = argv[1];
   }
-  else { //catches incorrect directories or errors
-    printf("Cannot open dir '%s'!", argv[1]);
-  }
+  // else { //catches incorrect directories or errors
+  //   printf("Cannot open dir '%s'!", argv[2]);
+  // }
 
   // Open directory
-  od = opendir(dir_name);
+  DIR *od = opendir(dir_name);
+  if (od == NULL) {
+    printf("Directory '%s' cannot be opened!\n", argv[1]);
+    exit(1);
+  }
 
   // Repeatedly read and print entries
   while ((enter = readdir(od)) != NULL)
